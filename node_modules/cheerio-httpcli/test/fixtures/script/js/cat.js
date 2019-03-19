16f05e0e077fe41bdeb6fc0129e08c56a738e727
Yuki <yutaroud@yahoo.co.jp>
2019-03-19 11:51:27 +0900

    該当フォルダのgit管理下から排除

--- node_modules/cheerio-httpcli/test/fixtures/script/js/cat.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = function () {
-  var cat = 'cat';
-  return cat;
-};
-
--- node_modules/cheerio-httpcli/test/fixtures/script/js/dog.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = function () {
-  var dog = 'dog';
-  return dog;
-};
